71b45731b5f80dcfe4af12d59770223530c9e61c
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-06-12 20:20:59 +0300

    Update Home.tsx

--- minimalizm/client/src/pages/Home.tsx
+++ minimalizm/client/src/pages/Home.tsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
 export const Home = () => {
-	  return (
-	<div>
-	  <h1>Home</h1>
-	  <p>This is the home page</p>
-	</div>
-  );
+  const { loading, error, data } = useFetch(
+    'http://localhost:1337/api/products',
+  )
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :( </p>
+  return (
+    <div>
+      {data.data.map((el: any) => (
+        <div key={el.id + 'key'}>
+          <span>{el.attributes.title}</span>
+
+          <p>{el.attributes.description}</p>
+          <Link to={`/details/${el.id}`}>Link to details</Link>
+        </div>
+      ))}
+    </div>
+  )
 }
