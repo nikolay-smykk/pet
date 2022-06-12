f9329bbd86b32ec2c3acae6a40c69bfb73f281b1
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-06-12 21:47:35 +0300

    Update ReviewDetails.tsx

--- minimalizm/client/src/pages/ReviewDetails.tsx
+++ minimalizm/client/src/pages/ReviewDetails.tsx
@@ -1,8 +1,26 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
+
 export const ReviewDetails = () => {
-	  return (
-	<div>
-	  <h1>Review Details</h1>
-	  <p>This is the review details page</p>
-	</div>
-  );
+  const { detailsId } = useParams()
+  const { data, error, loading } = useFetch(
+    `http://localhost:1337/api/products/${detailsId}`,
+  )
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :( </p>
+
+  console.log(data.data.attributes.img.data[0].attributes.url)
+  return (
+    <div>
+      <h1>{data.data.attributes.title}</h1>
+      <p>{data.data.attributes.description}</p>
+      <img
+        src={
+          'http://localhost:1337' +
+          data.data.attributes.img.data[0].attributes.url
+        }
+        alt={data?.data?.attributes?.title}
+      />
+    </div>
+  )
 }
