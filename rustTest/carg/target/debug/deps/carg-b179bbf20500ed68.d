0b93c6d8161e7dcf522f16fc08ffab53edb8525b
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-06-23 12:26:53 +0300

    init

---
+++ rustTest/carg/target/debug/deps/carg-b179bbf20500ed68.d
@@ -0,0 +1,5 @@
+/Users/nikolajsmyk/Documents/GitHub/pet/rustTest/carg/target/debug/deps/carg-b179bbf20500ed68.rmeta: src/main.rs
+
+/Users/nikolajsmyk/Documents/GitHub/pet/rustTest/carg/target/debug/deps/carg-b179bbf20500ed68.d: src/main.rs
+
+src/main.rs:
---
+++ rustTest/carg/target/debug/deps/carg-efef066522ecb439.d
@@ -0,0 +1,5 @@
+/Users/nikolajsmyk/Documents/GitHub/pet/rustTest/carg/target/debug/deps/carg-efef066522ecb439.rmeta: src/main.rs
+
+/Users/nikolajsmyk/Documents/GitHub/pet/rustTest/carg/target/debug/deps/carg-efef066522ecb439.d: src/main.rs
+
+src/main.rs:
