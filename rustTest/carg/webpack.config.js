dba317a89e51e1da25120681c2f9a60774a534e4
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-06-24 03:17:18 +0300

    Update webpack.config.js

--- rustTest/carg/webpack.config.js
+++ rustTest/carg/webpack.config.js
@@ -1,3 +1,13 @@
+const path = require('path')
+const HTMLWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-	
+	entry: ".public/main.js",
+	output: {
+		path : path.resolve(__dirname, 'dist'),
+		filename:"index.js"
+	},
+	plugin: [
+		new HTMLWebPackPlugin()
+	]
 }
