ef1dc30e1f03306549f26afd6d9d337ccadc7237
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-06-26 23:41:20 +0300

    prettier

--- rustTest/carg/public/main.js
+++ rustTest/carg/public/main.js
@@ -1,1 +1,10 @@
-console.log("some")
+function init() {
+  const input = document.getElementById("upload");
+
+  const fileReader = new FileReader();
+
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+init();
