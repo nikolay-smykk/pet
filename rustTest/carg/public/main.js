745a92539a6e09df54f04fec064a8b323913190a
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-06-28 19:48:14 +0300

    init

--- rustTest/carg/public/main.js
+++ rustTest/carg/public/main.js
@@ -2,6 +2,14 @@ function init() {
   const input = document.getElementById("upload");
 
   const fileReader = new FileReader();
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    console.log("input.files[0]", input.files[0]);
+    console.log("base64", base64);
+  };
 
   input.addEventListener("change", () => {
     fileReader.readAsDataURL(input.files[0]);
