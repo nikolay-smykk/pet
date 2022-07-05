5317702e36221ed5c77ab7ad486b97c99a447f52
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-07-05 18:22:03 +0300

    init

--- fastapi/routes/items.js
+++ fastapi/routes/items.js
@@ -1,11 +1,46 @@
-const fastify = require("fastify")({ logger: true });
-const items = require("./items");
+const items = require("../src/items");
+const uuidv4 = require("uuid").v4;
+// OPTIONS
+const uuid = uuidv4();
+
+const Item = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+    price: { type: "string" },
+    key: { type: "string", default: uuidv4() }
+  }
+};
+const getItemsOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: { ...Item }
+      }
+    }
+  }
+};
+
+const getItemOpts = {
+  schema: {
+    response: {
+      200: {
+        ...Item
+      }
+    }
+  }
+};
 
 function itemRoutes(fastify, options, done) {
-  fastify.get("/items", async (request, reply) => {
+  // GET ALL ITEMS
+  fastify.get("/items", getItemsOptions, async (request, reply) => {
     reply.send(items);
   });
-  fastify.get("/items/:id", async (request, reply) => {
+
+  // GET SINGLE ITEM
+  fastify.get("/items/:id", getItemOpts, async (request, reply) => {
     const { id } = request.params;
     const item = items.find(item => item.id === Number(id));
     if (!item) {
--- fastapi/src/index.js
+++ fastapi/src/index.js
@@ -1,8 +1,8 @@
+const fastify = require("fastify")({ logger: true });
+
+fastify.register(require("../routes/items"));
 
 const PORT = 5000;
-
-
-
 
 const start = async () => {
   try {
